395c4ecb2c72db1f5667d9656a0d02c92ac87a90
이지원 <gohead12@koreatech.ac.kr>
2024-04-30 12:29:31 +0900

    Update Programmers1.java

--- src/programmers/Programmers1.java
+++ src/programmers/Programmers1.java
@@ -5,10 +5,12 @@ class Programmers1 {
         int answer = 0;
         int[][] arr = new int[friends.length][friends.length];
 
+        //준 사람과 받은 사람을 " "을 기준으로 나눠서 저장
         for (String str : gifts) {
             String Jun = str.split(" ")[0];
             String Bab = str.split(" ")[1];
 
+            //준 사람과 받은 사람을 equals 함수를 이용하여 arr인덱스를 찾아서 증감
             for (int i = 0; i < friends.length; i++) {
                 if (Jun.equals(friends[i])){
                     for (int j = 0; j < friends.length; j++) {
@@ -18,6 +20,7 @@ class Programmers1 {
             }
         }
 
+        //선물 점수를 계산하여 giftIndex 배열에 저장
         int[] giftIndex = new int[friends.length];
         for (int i = 0; i < friends.length; i++) {
             int Jun = 0, Bab = 0;
@@ -28,6 +31,7 @@ class Programmers1 {
             giftIndex[i] = Jun - Bab;
         }
 
+        //위에서 계산한 arr 2차원 배열과 giftIndex 배열을 이용하여 monthGift 배열에 다음 달 선물 갯수를 계산하여 저장
         int[] monthGift = new int[friends.length];
         for (int i = 0; i < friends.length; i++) {
             for (int j = 0; j < friends.length; j++) {
@@ -43,6 +47,7 @@ class Programmers1 {
             }
         }
 
+        //monthGift의 제일 큰 수를 찾아 answer에 대입
         for(int i = 0; i <friends.length; i++) {
             if (monthGift[i] > answer) answer = monthGift[i];
         }
