eb311807daf37a90a45f294ba3f8a03fe418014c
이지원 <vryez11@gmail.com>
2024-05-04 09:34:51 +0900

    신규아이디 추천
    신규 아이디 추천 (2021 KAKAO BLIND RECRUITMENT)

--- src/Example.java
+++ src/Example.java
@@ -2,11 +2,14 @@ package src;
 
 import src.programmers.Programmers8;
 import src.programmers.Programmers9;
+import src.programmers.Programmers10;
 
 public class Example {
     public static void main(String[] args) {
-        Programmers9 solution = new Programmers9();
+        Programmers10 solution = new Programmers10();
 
-        int[][] result = solution.solution(4);
+        String result = solution.solution("-.~!@#$%&*()=+[{]}:?,<>/.-");
+
+        System.out.println(result);
     }
 }
