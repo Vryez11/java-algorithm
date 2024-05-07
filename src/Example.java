9f5171950dd2de583ff002be7e763818c5e6f28f
이지원 <vryez11@gmail.com>
2024-05-07 11:08:33 +0900

    12 데이터분석
    데이터분석 PCCP 기출 10번

--- src/Example.java
+++ src/Example.java
@@ -4,13 +4,14 @@ import src.programmers.Programmers8;
 import src.programmers.Programmers9;
 import src.programmers.Programmers10;
 import src.programmers.Programmers11;
+import src.programmers.Programmers12;
 
 public class Example {
     public static void main(String[] args) {
-        String[] surveys = {"AN", "CF", "MJ", "RT", "NA"};
-        int[] choises = {5, 3, 2, 7, 5};
-        String result = Programmers11.solution(surveys, choises);
+        int[][] data = {{1, 20300104, 100, 80},
+                        {2, 20300804, 847, 37},
+                        {3, 20300401, 10, 8}};
 
-        System.out.println("result = " + result);
+        int[][] result = Programmers12.solution(data, "date", 20300501, "remain");
     }
 }
