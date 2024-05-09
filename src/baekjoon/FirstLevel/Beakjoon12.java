0a8152d2e978095b1d15da6430d1b7dc87bf9e23
이지원 <vryez11@gmail.com>
2024-05-09 16:14:25 +0900

    단계별로 풀기 1단계 11~13번
    단계별로 풀기 1단계 11~13번

---
+++ src/baekjoon/FirstLevel/Beakjoon12.java
@@ -0,0 +1,12 @@
+package src.baekjoon.FirstLevel;
+
+public class Beakjoon12 {
+
+    public static void main(String[] args) {
+
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
