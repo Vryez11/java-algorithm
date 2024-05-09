24839e538235f9fa5d79bbb58fde02293c95175a
이지원 <vryez11@gmail.com>
2024-05-09 17:51:44 +0900

    단계별로풀기 2단계
    2단계 1 ~ 7번

---
+++ src/baekjoon/SecondLevel/Baekjoon15.java
@@ -0,0 +1,18 @@
+package src.baekjoon.SecondLevel;
+
+import java.util.Scanner;
+
+public class Baekjoon15 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int A = scanner.nextInt();
+
+        if (A >= 90) System.out.println("A");
+        else if (A >=80) System.out.println("B");
+        else if (A >= 70) System.out.println("C");
+        else if (A >= 60) System.out.println("D");
+        else System.out.println("F");
+    }
+}
---
+++ src/baekjoon/SecondLevel/Baekjoon17.java
@@ -0,0 +1,4 @@
+package src.baekjoon.SecondLevel;
+
+public class Baekjoon17 {
+}
