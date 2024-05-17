69a4b54e3c8d966f3a3dc8c0ffac00f09831b3e5
이지원 <vryez11@gmail.com>
2024-05-17 10:46:46 +0900

    백준 4단계풀이
    백준 4단계 풀이 (1 ~ 5)

---
+++ src/baekjoon/ForthLevel/Baekjoon35.java
@@ -0,0 +1,23 @@
+package src.baekjoon.ForthLevel;
+
+import java.util.Scanner;
+
+public class Baekjoon35 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int maxInt = 0;
+        int maxIndex = 0;
+        int tempInt = 0;
+
+        for (int i = 1; i <= 9; i++) {
+            tempInt = scanner.nextInt();
+            maxIndex = maxInt < tempInt ? i : maxIndex;
+            maxInt = maxInt < tempInt ? tempInt : maxInt;
+        }
+
+        System.out.println(maxInt);
+        System.out.println(maxIndex);
+    }
+}
