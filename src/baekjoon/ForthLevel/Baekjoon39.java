e3209797ceb114ddf7c840c2e44cb89cf2eefd4b
이지원 <vryez11@gmail.com>
2024-05-20 09:43:46 +0900

    4단계 별로 풀기
    4단계 별로 풀기 코드 추가

---
+++ src/baekjoon/ForthLevel/Baekjoon39.java
@@ -0,0 +1,33 @@
+package src.baekjoon.ForthLevel;
+
+import java.util.Scanner;
+
+public class Baekjoon39 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[] arrNum = new int[10];
+        int[] arrMod = new int[10];
+        int count = 0;
+
+        for (int i = 0; i < arrNum.length; i++) {
+            arrNum[i] = scanner.nextInt();
+        }
+
+        for (int j = 0; j < arrMod.length; j++) {
+            arrMod[j] = 42 % arrNum[j];
+        }
+
+//        for (int k = 0; k < arrMod.length; k++) {
+//            for (int m = k; m < arrMod.length; m++) {
+//                if (arrMod[k] != arrMod[m]) {
+//                    count++;
+//                    break;
+//                }
+//            }
+//        }
+
+        System.out.println(count);
+    }
+}
