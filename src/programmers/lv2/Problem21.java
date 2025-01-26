d05b50f78043273ec99cdf2f471837fbf726469b
이지원 <168502753+Vryez11@users.noreply.github.com>
2025-01-27 01:24:30 +0900

    doit
    bellmanford Problem60 add

---
+++ src/programmers/lv2/Problem21.java
@@ -0,0 +1,16 @@
+package src.programmers.lv2;
+
+public class Problem21 {
+
+    public int[] solution(int n, long left, long right) {
+
+        int[][] arr = new int[n + 1][n + 1];
+        for (int i = 1; i <= n; i++) {
+            int indexX = 1;
+            int indexY = 1;
+
+        }
+
+        return null;
+    }
+}
