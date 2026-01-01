ba0b36113c61939d0b5fd03467271cfbf77c9911
이지원 <168502753+Vryez11@users.noreply.github.com>
2026-01-01 12:30:59 +0900

    feat: 백준 20단계 백트래킹 - Baekjoon145 15649번 문제 해결

---
+++ src/baekjoon/level/twentieth/Baekjoon145.java
@@ -0,0 +1,51 @@
+package src.baekjoon.level.twentieth;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Baekjoon145 {
+
+    static int N, M;
+    static boolean[] used;
+    static int[] arr;
+    static StringBuilder sb = new StringBuilder();
+
+    static void dfs(int depth) {
+        if (depth == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(arr[i]);
+                if (i < M - 1) sb.append(' ');
+            }
+            sb.append('\n');
+            return;
+        }
+
+        for (int i = 1; i <= N; i++) {
+            if (!used[i]) {
+                used[i] = true;
+                arr[depth] = i;
+                dfs(depth + 1);
+                used[i] = false;
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        used = new boolean[N + 1];
+        arr = new int[M];
+
+        dfs(0);
+
+        System.out.print(sb.toString());
+    }
+}
