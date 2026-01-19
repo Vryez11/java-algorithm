f77e65c988abafa015d5a0dfee765b07cb6fb356
이지원 <168502753+Vryez11@users.noreply.github.com>
2026-01-20 01:19:57 +0900

    feat: 백준 21단계 동적프로그래밍
    - Baekjoon162 10844번
      - 끝자리 수를 기준으로 동적프로그래밍 생각해보기

---
+++ src/baekjoon/level/twentyfirst/Baekjoon182.java
@@ -0,0 +1,37 @@
+package src.baekjoon.level.twentyfirst;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Baekjoon182 {
+
+    static final long mod = 1_000_000_000;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        br.close();
+
+        long[][] D = new long[N + 1][11];
+        for (int i = 1; i <= 9; i++) {
+            D[1][i] = 1;
+        }
+
+        for (int i = 2; i <= N; i++) {
+            D[i][0] = D[i - 1][1];
+            D[i][9] = D[i - 1][8];
+            for (int j = 1; j <= 8; j++) {
+                D[i][j] = (D[i - 1][j - 1] + D[i - 1][j + 1]) % mod;
+            }
+        }
+
+        long sum = 0;
+        for (int i = 0; i < 10; i++) {
+            sum = (sum + D[N][i]) % mod;
+        }
+        System.out.println(sum);
+    }
+}
