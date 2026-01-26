345086e035e096f23c2093326c8837a4eab69d21
이지원 <168502753+Vryez11@users.noreply.github.com>
2026-01-27 01:11:23 +0900

    feat: 백준 23단계 그리드 알고리즘1
    - Baekjoon176 1931번 문제
      - 끝나는 시간을 기준으로 최선의 선택을 진행

---
+++ src/baekjoon/level/twentythird/Baekjoon176.java
@@ -0,0 +1,38 @@
+package src.baekjoon.level.twentythird;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class Baekjoon176 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[][] meetings = new int[N][2];
+        for (int i = 0; i < N; i++) {
+            meetings[i][0] = sc.nextInt();
+            meetings[i][1] = sc.nextInt();
+        }
+
+        Arrays.sort(meetings, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if (o1[1] == o2[1])
+                    return o1[0] - o2[0];
+                return o1[1] - o2[1];
+            }
+        });
+
+        int count = 0;
+        int end = -1;
+        for (int i = 0; i < N; i++) {
+            if (meetings[i][0] >= end) {
+                end = meetings[i][1];
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
