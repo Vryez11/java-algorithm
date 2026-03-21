deded13b3b708863222d11fd8d4143573f1aa27b
이지원 <168502753+Vryez11@users.noreply.github.com>
2026-03-21 21:05:10 +0900

    feat: 프로그래머스 Lv2 Problem90Res
    - 비밀 코드 해독 문제 풀이
      - DFS 1부터 n까지 5개 숫자 조합 생성
      - 각 조합을 q, ans와 바로 비교해 가능한 비밀 코드 계산

--- src/programmers/lv2/Problem90Res.java
+++ src/programmers/lv2/Problem90Res.java
@@ -1,4 +1,46 @@
 package src.programmers.lv2;
 
 public class Problem90Res {
+
+    int answer = 0;
+    int[] selected = new int[5];
+
+    public int solution(int n, int[][] q, int[] ans) {
+        dfs(n, 1, 0, q, ans);
+        return answer;
+    }
+
+    private void dfs(int n, int start, int depth, int[][] q, int[] ans) {
+        if (depth == 5) {
+            if (isValid(q, ans)) {
+                answer++;
+            }
+            return;
+        }
+
+        for (int i = start; i <= n; i++) {
+            selected[depth] = i;
+            dfs(n, i + 1, depth + 1, q, ans);
+        }
+    }
+
+    private boolean isValid(int[][] q, int[] ans) {
+        for (int i = 0; i < q.length; i++) {
+            int count = 0;
+
+            for (int j = 0; j < 5; j++) {
+                for (int k = 0; k < 5; k++) {
+                    if (selected[j] == q[i][k]) {
+                        count++;
+                    }
+                }
+            }
+
+            if (count != ans[i]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
 }
