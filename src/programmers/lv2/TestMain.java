d604a6f0484788816144684d3eaf3d735d532e88
이지원 <168502753+Vryez11@users.noreply.github.com>
2026-03-23 21:00:32 +0900

    feat: 프로그래머스 Lv2 Problem91 Test
    - 프로그래머스 Lv2 Problem91
      - Test코드 진행

--- src/programmers/lv2/TestMain.java
+++ src/programmers/lv2/TestMain.java
@@ -7,7 +7,7 @@ public class TestMain {
     public static void main(String[] args) {
 
         Problem91 problem91 = new Problem91();
-        String[] solution = problem91.solution(new String[][]{{"korean", "12:10", "30"}, {"english", "11:40", "20"}});
+        String[] solution = problem91.solution(new String[][]{{"korean", "12:10", "30"}, {"english", "11:40", "20"}, {"japan", "13:50", "40"}});
         System.out.println("solution = " + solution);
     }
 }
