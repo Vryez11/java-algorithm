f416f812287dc9a887437d65ea63db711a436fdc
이지원 <168502753+Vryez11@users.noreply.github.com>
2026-03-25 18:07:50 +0900

    feat: 프로그래머스 Lv2 Problem93
    - 후보키
      - 비트마스크를 활용해 컬럼 조합 생성
      - Set으로 각 조합의 유일성 검사
      - 기존 후보키 포함 여부로 최소성 검사

---
+++ src/programmers/lv2/Problem93.java
@@ -0,0 +1,47 @@
+package src.programmers.lv2;
+
+import java.util.*;
+
+public class Problem93 {
+
+    // [후보키]
+    public int solution(String[][] relation) {
+        int rowCount = relation.length;
+        int colCount = relation[0].length;
+
+        List<Integer> candidateKeys = new ArrayList<>();
+        
+        for (int bit = 1; bit < (1 << colCount); bit++) {
+
+            boolean isMinimal = true;
+            for (int key : candidateKeys) {
+                if ((bit & key) == key) {
+                    isMinimal = false;
+                    break;
+                }
+            }
+
+            if (!isMinimal) continue;
+
+            Set<String> set = new HashSet<>();
+
+            for (int i = 0; i < rowCount; i++) {
+                StringBuilder sb = new StringBuilder();
+
+                for (int j = 0; j < colCount; j++) {
+                    if ((bit & (1 << j)) != 0) {
+                        sb.append(relation[i][j]).append(",");
+                    }
+                }
+
+                set.add(sb.toString());
+            }
+
+            if (set.size() == rowCount) {
+                candidateKeys.add(bit);
+            }
+        }
+
+        return candidateKeys.size();
+    }
+}
