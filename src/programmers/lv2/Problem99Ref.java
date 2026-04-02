1e96eaa58ef547e1bacdd60d916feec92a7fbb80
이지원 <168502753+Vryez11@users.noreply.github.com>
2026-04-02 15:52:41 +0900

    fix: 프로그래머스 Lv2 Problem99Ref
    - 숫자 블록 리팩토링
      - 약수를 구할 때 sqrt(num)으로 제한 후 작은 수의 pair를 배열에 저장
      - pair가 10_000_000보다 크면 i를 저장 하도록 구현
      - 약수 구하는 패턴을 상기했음(int i = 0; i * i <= num; i++)

--- src/programmers/lv2/Problem99Ref.java
+++ src/programmers/lv2/Problem99Ref.java
@@ -1,4 +1,39 @@
 package src.programmers.lv2;
 
 public class Problem99Ref {
+
+    // 숫자 블록 리팩토링
+    public int[] solution(long begin, long end) {
+
+        int[] answer = new int[(int) (end - begin + 1)];
+        int idx = 0;
+
+        for (long num = begin; num <= end; num++) {
+            if (num == 1) {
+                answer[idx++] = 0;
+                continue;
+            }
+
+            answer[idx++] = findBlock(num);
+        }
+
+        return answer;
+    }
+
+    private int findBlock(long num) {
+        int result = 1;
+
+        for (long i = 2; i * i <= num; i++) {
+            if (num % i == 0) {
+                long pair = num / i;
+                if (pair <= 10_000_000) {
+                    return (int) pair;
+                }
+
+                result = (int) i;
+            }
+        }
+
+        return result;
+    }
 }
