9960b25ffb5a427403ae802f350f821e0cd65ccf
이지원 <168502753+Vryez11@users.noreply.github.com>
2026-04-05 13:30:36 +0900

    feat: 프로그래머스 Lv3 Problem2
    - 정수 삼각형 문제 풀이
      - DP 문제 풀이
        - DP의 핵심은 이전의 값에서 현재 최선의 값 찾기
        - DP[][] 배열의 의미와 점화식을 어떻게 세울지
        - 초기값 설정

---
+++ src/programmers/lv3/Problem2.java
@@ -0,0 +1,28 @@
+package src.programmers.lv3;
+
+public class Problem2 {
+
+    // 정수 삼각형
+    public int solution(int[][] triangle) {
+        int n = triangle.length;
+        int[][] dp = new int[n][n];
+
+        dp[0][0] = triangle[0][0];
+
+        for (int i = 1; i < n; i++) {
+            dp[i][0] = dp[i-1][0] + triangle[i][0];
+            dp[i][i] = dp[i-1][i-1] + triangle[i][i];
+
+            for (int j = 1; j < i; j++) {
+                dp[i][j] = Math.max(dp[i-1][j-1], dp[i - 1][j]) + triangle[i][j];
+            }
+        }
+
+        int answer = 0;
+        for (int j = 0; j < n; j++) {
+            answer = Math.max(answer, dp[n-1][j]);
+        }
+
+        return answer;
+    }
+}
