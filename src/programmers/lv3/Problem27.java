1617f0e4c4c412ec1a8ac9584f1405b99fcc4be5
이지원 <168502753+Vryez11@users.noreply.github.com>
2026-05-09 07:55:26 +0900

    feat: 프로그래머스 Lv3 Problem27
    - 풍선 터트리기 문제 풀이
      - 그리디 문제
      - 왼쪽에 작은 숫자가 없거나 오른쪽에 작은 숫자가 없다면 최후까지 남을 수 있다.

---
+++ src/programmers/lv3/Problem27.java
@@ -0,0 +1,39 @@
+package src.programmers.lv3;
+
+public class Problem27 {
+
+    // 풍선 터트리기
+    public int solution(int[] a) {
+
+        int n = a.length;
+
+        boolean[] possible = new boolean[n];
+
+        int min = Integer.MAX_VALUE;
+
+        for (int i = 0; i < n; i++) {
+            if (a[i] < min) {
+                possible[i] = true;
+                min = a[i];
+            }
+        }
+
+        min = Integer.MAX_VALUE;
+
+        for (int i = n - 1; i >= 0; i--) {
+            if (a[i] < min) {
+                possible[i] = true;
+            }
+        }
+
+        int answer = 0;
+
+        for (boolean canSurvive : possible) {
+            if (canSurvive) {
+                answer++;
+            }
+        }
+
+        return answer;
+    }
+}
