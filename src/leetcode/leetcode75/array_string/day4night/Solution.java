fb5404379a34c42fab849d67f0f486cd34a45073
이지원 <168502753+Vryez11@users.noreply.github.com>
2026-06-04 16:49:15 +0900

    LeetCode75 day4Night 문제 풀이
    - Revers Vowels of a String 문제 풀이
      - 모음을 앞, 뒤 순서 반전 시키는 문제
      - Stack을 이용해서 첫 순회 때 모음 넣기 두 번째 순횐 때 꺼내서 반전 모음 넣기

---
+++ src/leetcode/leetcode75/array_string/day4night/Solution.java
@@ -0,0 +1,37 @@
+package src.leetcode.leetcode75.array_string.day4night;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class Solution {
+
+    // Revers Vowels of a String
+    public String reverseVowels(String s) {
+
+        Deque<Character> stack = new ArrayDeque<>();
+
+        char[] chars = s.toCharArray();
+        for (char ch : chars) {
+            if (isVowel(ch)) stack.push(ch);
+        }
+
+        for (int i = 0; i < chars.length; i++) {
+            if (!isVowel(chars[i])) continue;
+
+            Character nowCh = stack.pop();
+            chars[i] = nowCh;
+        }
+
+        return String.valueOf(chars);
+    }
+
+    private boolean isVowel(char ch) {
+        ch = Character.toLowerCase(ch);
+
+        return ch == 'a'
+                || ch == 'e'
+                || ch == 'i'
+                || ch == 'o'
+                || ch == 'u';
+    }
+}
