6d77c42d4583903f178bc1e1a67ff0945d16d982
이지원 <168502753+Vryez11@users.noreply.github.com>
2026-06-05 10:13:27 +0900

    feat: LeetCode75 day5Moring 문제 해결
    - Reverse Words in a String 문제 풀이
      - 단어 역변환
      - 앞뒤 공백 제거 trim()
      - 단어를 spilt()으로 진행했을 때 여러 공백 있을 때 isEmpty()로 한 번 거르기

---
+++ src/leetcode/leetcode75/array_string/day5morning/Solution.java
@@ -0,0 +1,27 @@
+package src.leetcode.leetcode75.array_string.day5morning;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class Solution {
+
+    // Reverse Words in a String
+    public String reverseWords(String s) {
+
+        String noTrimeString = s.trim();
+        String[] words = noTrimeString.split(" ");
+        Deque<String> stack = new ArrayDeque<>();
+        for (String word : words) {
+            if (word.isEmpty()) continue;
+
+            stack.push(word.trim());
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while (!stack.isEmpty()) {
+            sb.append(stack.pop()).append(" ");
+        }
+
+        return sb.toString().trim();
+    }
+}
