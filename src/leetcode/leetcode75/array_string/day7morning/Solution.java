c16ce5ce9850baeee590ffc8e8df61bd86280b83
이지원 <168502753+Vryez11@users.noreply.github.com>
2026-06-07 12:25:38 +0900

    feat: LeetCode75 day7Morning
    - Increasing Triplet Subsequence
      - i < j < k && nums[i] < nums[j] < nums[k] 를 만족하는 게 있는지 찾는 문제
      - nums 기준 첫번 쨰 if에 first 비교 두번 째 else if 로 second 비교 이 후 else 2개 통과하면 return true;

---
+++ src/leetcode/leetcode75/array_string/day7morning/Solution.java
@@ -0,0 +1,24 @@
+package src.leetcode.leetcode75.array_string.day7morning;
+
+public class Solution {
+
+    // Increasing Triplet Subsequence
+    public boolean increasingTriplet(int[] nums) {
+        if (nums.length < 3) return false;
+
+        int first = Integer.MAX_VALUE;
+        int second = Integer.MAX_VALUE;
+
+        for (int num : nums) {
+            if (num <= first) {
+                first = num;
+            } else if (num <= second) {
+                second = num;
+            } else {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
