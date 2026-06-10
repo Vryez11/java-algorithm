9451d0abd42d00b9b7bb425f8d39cf81999d9c95
이지원 <168502753+Vryez11@users.noreply.github.com>
2026-06-10 18:19:41 +0900

    feat: LeetCode75 day10
    - Is Subsequence 문제 풀이
      - sub 문자열이 t 문자열의 순서를 잘 따르는지 확인하는 문제
      - 투 포인터를 이용해서 sub 문자열 순서 체크 후 확인 반환

---
+++ src/leetcode/leetcode75/twopointers/day10/Solution.java
@@ -0,0 +1,22 @@
+package src.leetcode.leetcode75.twopointers.day10;
+
+public class Solution {
+
+    // Is Subsequence
+    public boolean isSubsequence(String s, String t) {
+
+        int sIdx = 0;
+        int tIdx = 0;
+
+        while (sIdx < s.length() && tIdx < t.length()) {
+
+            if (s.charAt(sIdx) == t.charAt(tIdx)) {
+                sIdx++;
+            }
+
+            tIdx++;
+        }
+
+        return sIdx >= s.length();
+    }
+}
